95895d2295b6a0642d15248d84860352914063bc
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2021-02-26 11:44:50 -0300

    reading from file
    also removed duplicated files

---
+++ MKPInstance.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class MKPInstance:
+    def __init__(self, file_path):
+        self._read_instance_from_file(file_path)
+
+    def _read_instance_from_file(self, file_path):
+        # The format of these data files is:
+        # Variables (n), #Constraints (m), Optimal value (0 if unavailable)
+        # Profit P(j) for each n
+        # n x m matrix of constraints
+        # Capacity b(i) for each m
+        instance = []
+        f = open(file_path, "r")
+        for line in f:
+            for i in line.split():
+                instance.append(int(i))
+
+        self.n = instance[0]
+        self.m = instance[1]
+        self.optmal_value = instance[2]
+        self.p = instance[3 : 3 + self.n]
+        self.constraints_matrix = np.array(
+            instance[3 + self.n : 3 + self.n + self.n * self.m]
+        ).reshape(self.n, self.m)
+        self.b = instance[
+            3 + self.n + self.n * self.m : 3 + self.n + self.n * self.m + self.m
+        ]
+        # return (n, m, optmal_value, p, constraints_matrix, b)
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def read_instance():
-    pass
+from MKPInstance import MKPInstance
+
+i = MKPInstance("instances-chubeas/OR10x100/OR10x100-0.25_1.dat")
+
+print(i.n)
+print(i.m)
+print(i.optmal_value)
+print(i.p)
+print(i.constraints_matrix)
+print(i.b)
