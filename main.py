0e50b9e9eb26e5f5ef8a46caaa936120b9ae3157
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2021-03-07 19:05:55 -0300

    running all tests

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from CustomMKH import CustomMKH
 from MKPInstance import MKPInstance
 from MKHEUR import MKHEUR
 from CustomMKH import CustomMKH
+import os
 
 instances = []
 
+for root, dirs, files in os.walk("instances-chubeas", topdown=False):
+    for name in files:
+        instances.append(MKPInstance(os.path.join(root, name)))
 
-instance = MKPInstance("instances-chubeas/OR30x500/OR30x500-0.25_1.dat")
+for instance in instances:
+    print("m=" + str(instance.m))
+    print("n=" + str(instance.n))
+    mkheur = MKHEUR(instance)
+    custom = CustomMKH(instance)
 
-mkheur = MKHEUR(instance)
-custom = CustomMKH(instance)
-
-result = mkheur.solve()
-print(result)
-result2 = custom.solve()
-print(result2)
+    result = mkheur.solve()
+    print("obj_fun=" + str(result[0]))
+    print("time=" + str(result[1]))
+    result2 = custom.solve()
+    print("mod_obj_fun=" + str(result2[0]))
+    print("mod_time=" + str(result2[1]))
